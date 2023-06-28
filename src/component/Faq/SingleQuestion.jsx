5d5bf01d5b3130c106417f9224d6262f8bd75d42
Joshua77 <jpakinola@gmail.com>
2023-06-28 12:29:14 +0100

    more changes

--- src/component/Faq/SingleQuestion.jsx
+++ src/component/Faq/SingleQuestion.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ui";
+//import {AiOutlineMinus} from "react-icons/ui";
+import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
 
 const SingleQuestion = ({title, info})=>{
 //core functionality is to either show or hide the info
 //note that we set the button to return the opposite of the current state anytime we click on it
 //so if it was open/true now, clicking it will make it return the opposite which is to close Info
+
+
+// { showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/> }
+
+
     const [showInfo, setShowInfo] =useState(false);
 
     return(
@@ -12,7 +18,7 @@ const SingleQuestion = ({title, info})=>{
             <header>
                 <h4>{title}</h4>
                 <button onClick={()=> setShowInfo(!setShowInfo)}>
-                    { showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/> }
+                   
                 </button>
             </header>
             { showInfo && <p> { info }</p>}
